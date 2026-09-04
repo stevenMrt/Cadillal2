2d6716db3cb88a45542610b170ac0b0b99595204
Developer <dev@localhost>
2026-09-04 12:15:46 -0500

    Register User model in Django Admin for production

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('role', 'is_active', 'is_staff', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    ordering = ('-date_joined',)
+    fields = ('email', 'username', 'first_name', 'last_name', 'role', 'phone', 'document', 'birth_date', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+    readonly_fields = ('date_joined',)
